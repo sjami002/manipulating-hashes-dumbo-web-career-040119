8296ed29978613076f70b2304319c5089cc955bd
Shoptorshi Jamil <sjami002@gmail.com>
2019-03-15 23:00:13 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -17,6 +17,8 @@ def first_challenge
   array = contacts["Freddy Mercury"][:favorite_icecream_flavors]
   array.each do |value|
     value.delete_if {|flavor| flavor == "strawberry"}
+  end
+end
 
   #remember to return your newly altered contacts hash!
   contacts
